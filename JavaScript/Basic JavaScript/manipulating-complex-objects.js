6390ca3245a6041125ff2f6b018fcc82b6fa5ce9
omatheusmesmo <matheus.6148@gmail.com>
2022-07-21 21:28:38 -0300

    novas lições 21-07-2022

---
+++ JavaScript/Basic JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Led Zeppelin",
+    "title": "Led Zeppelin IV",
+    "release_year": 1971,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
